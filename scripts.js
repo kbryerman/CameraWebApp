fb36d29bfb727b36365dfa37741647dfe7c90ecf
Karly Bryerman <karlybryerman@gmail.com>
2018-02-28 21:12:48 -0800

    Final Push

--- scripts.js
+++ scripts.js
@@ -20,14 +20,38 @@ $(".record").click(function () {
 
 $(".icon_hide").click(function () {
     $(".container2").hide();
+    $(".bottom_container2").hide();
     }); $(document).mousemove(function () {
-    $(".container2").fadeIn(250);
+    $(".container2").show().fadeIn(250);
+    $(".bottom_container2").show().fadeIn(250);
 });
     
-//$('.icon_expandScreen').click(function(e){
-//    // Use the plugin
-//    $('html').fullScreen();
-//    e.preventDefault();
-//});
-	
+$('.icon_expandScreen').click(function(){
+    $('body').fullscreen().request();
+    $('.titleHeader').hide();
+    $('.fullScreen_UI').css("display", "block");
 });
+
+$('.icon_exitFullScreen').click(function(){
+    $('body').fullscreen().cancel();
+    $('.titleHeader').show();
+    $('.fullScreen_UI').css("display", "none");
+});
+
+$('.icon_rotateLeft').click(function() {
+    var angle = ($('.icon_rotateA').data('angle') + 90) || 0;
+    $('.icon_rotateA').css({'transform': 'rotate(' + angle + 'deg)'});
+    $('.icon_rotateA').data('angle', angle);
+});
+    
+$('.icon_rotateRight').click(function() {
+    var angle = ($('.icon_rotateA').data('angle') - 90) || 0;
+    $('.icon_rotateA').css({'transform': 'rotate(' + angle + 'deg)'});
+    $('.icon_rotateA').data('angle', angle);
+});
+
+});
+
+
+    
+    
